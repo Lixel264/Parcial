29ccfd3d398d6bf19f5c0924eb1f574a3372b2a6
Stuart Estevez <stuart264.se@gmail.com>
2020-06-02 21:24:44 -0400

    Examen

---
+++ Examen1/Examen1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Examen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia;
+            Console.WriteLine("Dame un día de la semana.");
+            dia = Console.ReadLine();
+            switch (dia)
+            {
+                case "lunes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "martes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "miercoles":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "jueves":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "viernes":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                case "sabado":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                case "domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+                default:
+                    Console.WriteLine("Lo siento, no ingresaste un día de la semana");
+                    break;
+            }
+        }
+    }
+}
